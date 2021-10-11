4836ae7bad6fec905544128dd22d94c8c3801596
Akim Malyschik <AkimMalyschik@coherentsolutions.com>
2021-10-11 16:47:44 +0300

    fixed utf-8 decode error

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ def func(n1, n2):
 def main():
     your_variant = 10
     for n1 in range(1, 5):
-        with open(f'{n1}.txt') as f:
+        with open(f'{n1}.txt', encoding='utf-8') as f:
             for n2, name in enumerate(f.readlines()):
                 if func(n1, n2 + 1) == your_variant:
                     print(f'{n1} {name}')
